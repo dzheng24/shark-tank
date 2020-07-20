d37d8dccc1f5d7ae64e9881af717008e3188f5eb
David Zheng <zhengdav@msu.edu>
2020-07-20 10:19:03 -0700

    linked up create card page

--- index.js
+++ index.js
@@ -9,10 +9,15 @@ app.get("/", (req, res) => {
   res.render("welcome-page.ejs", { name: "David" });
 });
 
-app.get("/display", (req, res) => {
+app.get("/display-page", (req, res) => {
   res.render("display-page.ejs");
 });
 
+app.get("/create-card", (req, res) => {
+  res.render("create-card.ejs");
+});
+
+// -------------------------------------------------
 app.listen(3000, () => {
   console.log("server is up on 3000");
 });
