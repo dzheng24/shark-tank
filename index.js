04291f6a07be5f81cef403c3137b16085a99273f
David Zheng <zhengdav@msu.edu>
2020-07-20 09:59:04 -0700

    linked up display-page to welcome-page

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ app.get("/", (req, res) => {
   res.render("welcome-page.ejs", { name: "David" });
 });
 
+app.get("/display", (req, res) => {
+  res.render("display-page.ejs");
+});
+
 app.listen(3000, () => {
   console.log("server is up on 3000");
 });
