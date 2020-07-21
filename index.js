f3e56362dabae09e62089f261e5ac61c6a74e1f0
David Zheng <zhengdav@msu.edu>
2020-07-20 17:17:34 -0700

    added .env file

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
+// ----- Dependencies -----
 const express = require("express");
-const app = express();
+require("dotenv").config();
 const ejs = require("ejs");
 
+// ----- Start the Server -----
+const app = express();
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => console.log(`listening on port ${PORT}`));
+
 app.set("view-engine", "ejs");
+
+console.log(__dirname);
+app.use(express.static("js"));
 
 // ROUTES -----------------------------------------
 app.get("/", (req, res) => {
@@ -17,7 +26,13 @@ app.get("/create-card", (req, res) => {
   res.render("create-card.ejs");
 });
 
+// app.post("/create-card", (req, res) => {
+//   console.log("button clicked");
+//   res.send("clicked");
+// });
+
+// app.get("/public/js", (req, res) => {
+//   res.render("test.ejs");
+// });
+
 // -------------------------------------------------
-app.listen(3000, () => {
-  console.log("server is up on 3000");
-});
